ee4cb2a317626ad4e1cfd2d9183c0226985e06cc
Kenanpasayev <tu7kr6n7r@code.edu.az>
2024-04-18 06:20:29 +0400

    collections homework

---
+++ ConsoleApp14/CustomList.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    internal class CustomList<T>
+    {
+
+        T[] arr;
+        List<T> items;
+
+        public CustomList()
+        {
+            arr = Array.Empty<T>();
+
+        }
+        public void Add(T item)
+        {
+            Array.Resize(ref arr, arr.Length + 1);
+            arr[arr.Length - 1] = item;
+        }
+
+        public T Find(Predicate<T> value)
+        {
+            foreach (T item in items)
+            {
+                return items.Find(value);
+            }
+            return default(T);
+        }
+        public List<T> FindAll(Predicate<T> value)
+        {
+            return items.FindAll(value);
+        }
+        public bool Remove(T item)
+        {
+
+            return items.Remove(item);
+        }
+        public int RemoveAll(Predicate<T> value)
+        {
+            return items.RemoveAll(value);
+        }
+    }
+}
---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CustomList<string> list = new
+                CustomList<string>();
+            list.Add("salam");
+            list.Add("orxan");
+
+        }
+    }
+}
